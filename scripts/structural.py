d994aead29eadb4e44d31b63638e80b01ecfab22
Dingwen Qian <dqian0618@login2.ls6.tacc.utexas.edu>
2025-03-03 15:46:11 -0600

    tacc

--- scripts/structural.py
+++ scripts/structural.py
@@ -5,52 +5,126 @@ import matplotlib.pyplot as plt
 import MDAnalysis as mda
 import freud
 import shutil
-# Define calculation for direct structure factor
+import argparse
+import os
 
-def compute_sf_for_timestep_Direct(u,ts_index1,ts_index2,bins,reset=False):
-    ts=u.trajectory[ts_index1]
+# Define calculation for direct structure factor
+def compute_sf_for_timestep_Direct(u: mda.Universe, ts_index1: int, ts_index2: int, bins: int, reset: bool = False) -> freud.diffraction.StaticStructureFactorDirect:
+    """
+    Compute structure factor for a given time window using direct method
+    
+    Args:
+        u: MDAnalysis Universe object
+        ts_index1: Start timestep index
+        ts_index2: End timestep index
+        bins: Number of bins for structure factor calculation
+        reset: Whether to reset calculation between frames
+        
+    Returns:
+        freud.diffraction.StaticStructureFactorDirect object
+    """
+    ts = u.trajectory[ts_index1]
     box = ts.dimensions[:3]
-    deltak=4*np.pi/box[0]
-    k_max=(bins+0.5)*deltak
-    k_min=0.5*deltak
+    deltak = 4*np.pi/box[0]
+    k_max = (bins+0.5)*deltak
+    k_min = 0.5*deltak
+    
     sfDirect = freud.diffraction.StaticStructureFactorDirect(bins=bins, k_max=k_max, k_min=k_min)
-    for ts in range(ts_index1,ts_index2):
+    
+    for ts in range(ts_index1, ts_index2):
         ts = u.trajectory[ts]
         positions = u.atoms.positions.copy()
-        sfDirect.compute((box, positions),reset=reset)
-    return sfDirect
-
-# Load trajectory
-
-
-salt_list = [0.9, 1.7, 3, 9, 33, 66, 80]
-runstep = 1000
-for salt in salt_list:
+        sfDirect.compute((box, positions), reset=reset)
     
-    dataname=f"../data/{salt}mM_Dump.data"
-    trjname=f"../trajectories/{salt}mM_Dump.lammpstrj"
-    desttrjname=f"../trajectories/{salt}mM_Dump.lammpsdump"
-    shutil.copyfile(trjname,desttrjname)
-    # read the trajectory
-    u = mda.Universe(dataname, desttrjname)
-    bins = 30
+    return sfDirect
+def parse_arguments() -> argparse.Namespace:
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser(description='Calculate structure factor for a salt concentration')
+    parser.add_argument('--salt', type=float, required=True,
+                        help='Salt concentration in mM, e.g. 0.9')
+    parser.add_argument('--step_size', type=float, required=True,
+                       help='Step size for simulation')
+    parser.add_argument('--n_particles', type=int, required=True, 
+                       help='Number of particles')
+    parser.add_argument('--box_length', type=float, required=True,
+                       help='Box length L')
+    parser.add_argument('--total_time', type=float, required=True,
+                       help='Total simulation time')
+    parser.add_argument('--tau_crawl', type=float, required=True,
+                       help='Tau crawl parameter')
+    parser.add_argument('--freeroll', type=bool, required=True,
+                       help='Whether the simulation is freeroll')
+    return parser.parse_args()
+def load_trajectory(salt: float, step_size: float, n_particles: int, box_length: float, total_time: float, tau_crawl: float, freeroll: bool) -> mda.Universe:
+    """Load trajectory files and return MDAnalysis Universe"""
+    if freeroll:
+        dataname = f"../data/{salt}mM_freeroll_step{step_size}_N_{n_particles}_L_{box_length:.0f}_t_{total_time:.0f}_taucrawl_{tau_crawl:.0f}_Dump.data"
+        trjname = f"../trajectories/{salt}mM_freeroll_step{step_size}_N_{n_particles}_L_{box_length:.0f}_t_{total_time:.0f}_taucrawl_{tau_crawl:.0f}_Dump.lammpstrj"
+        desttrjname = f"../trajectories/{salt}mM_freeroll_step{step_size}_N_{n_particles}_L_{box_length:.0f}_t_{total_time:.0f}_taucrawl_{tau_crawl:.0f}_Dump.lammpsdump"
+        if os.path.exists(trjname):
+            shutil.move(trjname, desttrjname)
+    return mda.Universe(dataname, desttrjname)
+
+def save_structure_factors(u: mda.Universe, salt: float, step_size: float, n_particles: int, box_length: float, total_time: float, tau_crawl: float, freeroll: bool, bins: int = 30) -> None:
+    """Calculate, plot and save structure factors for different time windows"""
     start_index = np.arange(0, 1000, 100)
     end_index = np.arange(10, 1000+10, 100)
-    for j in range(0,10):
-        sfDirect = compute_sf_for_timestep_Direct(u,start_index[j],end_index[j],bins)
-        #plot and compare debye and direct
+    if freeroll:
+        nameprefix = f"{salt}mM_freeroll_step{step_size}_N_{n_particles}_L_{box_length:.0f}_t_{total_time:.0f}_taucrawl_{tau_crawl:.0f}"
+    # Create DataFrame to store structure factors
+    sf_data = []
+    
+    for j in range(0, 10):
+        sfDirect = compute_sf_for_timestep_Direct(u, start_index[j], end_index[j], bins)
         k_unit = sfDirect.bin_centers / 15
-        plt.plot(k_unit, sfDirect.S_k, label=f"{start_index[j]}")
-        #plt.plot(sfDebye.k_values, sfDebye.S_k, label="Debye")
-        plt.title("Static Structure Factor")
+        
+        # Store data
+        sf_data.append({
+            'timestep': start_index[j],
+            'k_values': k_unit,
+            'S_k': sfDirect.S_k
+        })
+        
+        plt.plot(k_unit, sfDirect.S_k, label=f"t={start_index[j]}")
+        
+        plt.title(f"Static Structure Factor - {salt}mM")
         plt.xlabel("$k (A^{-1})$")
         plt.ylabel("$S(k)$")
         plt.xscale('log')
         plt.yscale('log')
         plt.legend()
-        plt.savefig(f"../figures/salt_{salt}_time_{start_index[j]}_end_{end_index[j]}.png")
-        with open(f"../structural_factors/salt_{salt}_time_{start_index[j]}_end_{end_index[j]}.txt",'w') as f:
-            f.write(f"k_unit    S_k\n")
-            for m in range(0,len(k_unit)):
-                f.write(f"{k_unit[m]}   {sfDirect.S_k[m]}\n")
-    plt.clf()
+    
+        # Save figure
+        plt.savefig("../figures/"+nameprefix+f"_start{start_index[j]}_end{end_index[j]}.png", dpi=300, bbox_inches='tight')
+        plt.close()
+    
+    # Save structure factor data
+    for i, data in enumerate(sf_data):
+        df = pd.DataFrame({
+            'k_values': data['k_values'],
+            'S_k': data['S_k']
+        })
+        df.to_csv("../structural_factors/"+nameprefix+f"_start{start_index[i]}_end{end_index[i]}.csv", index=False)
+
+
+
+def main():
+    """Main function to run structure factor analysis"""
+    args = parse_arguments()
+    
+    # Create output directories if they don't exist
+    for dir_path in ['../figures', '../data']:
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+    
+    # Load trajectory
+    u = load_trajectory(args.salt, args.step_size, args.n_particles, args.box_length, args.total_time, args.tau_crawl, args.freeroll)
+    
+    # Calculate, plot and save structure factors
+    save_structure_factors(u, args.salt, args.step_size, args.n_particles, args.box_length, args.total_time, args.tau_crawl, args.freeroll)
+
+if __name__ == "__main__":
+    main()
+
+
+
--- tacc_run.sh
+++ tacc_run.sh
@@ -14,11 +14,18 @@
 
 # Any other commands must follow all #SBATCH directives...
 cd $SLURM_SUBMIT_DIR
+module load gcc/11.2.0  # If using a module system
 module load gsl
 module load boost
-./colloidgel -N 8000 -L 100 -G 25 -d 0.275 -D 9mM_freeroll_step0.1_N_8000_L_100 -m 0.1 &
-./colloidgel -N 8000 -L 100 -G 25 -d 0.459 -D 3mM_freeroll_step0.1_N_8000_L_100 -m 0.1 &
-./colloidgel -N 8000 -L 100 -G 25 -d 0.6375 -D 1.7mM_freeroll_step0.1_N_8000_L_100 -m 0.1 &
-./colloidgel -N 8000 -L 100 -G 25 -d 0.919 -D 0.9mM_freeroll_step0.1_N_8000_L_100 -m 0.1 &
-./colloidgel -N 8000 -L 100 -G 25 -d 0.0925 -D 80mM_freeroll_step0.1_N_8000_L_100 -m 0.1 &
+export PATH=/opt/apps/gcc/11.2.0/bin:$PATH
+export LD_LIBRARY_PATH=/opt/apps/gcc/11.2.0/lib64:$LD_LIBRARY_PATH
+export PATH=/scratch/tacc/apps/gcc11_2/boost/1.86.0/include:$PATH
+export LD_LIBRARY_PATH=/scratch/tacc/apps/gcc11_2/boost/1.86.0/lib:$LD_LIBRARY_PATH
+export PATH=/scratch/tacc/apps/gcc11_2/gsl/2.8/include:$PATH
+export LD_LIBRARY_PATH=/scratch/tacc/apps/gcc11_2/gsl/2.8/lib:$LD_LIBRARY_PATH
+./colloidgel -N 8000 -L 100 -G 25 -d 0.275 -D 9mM_freeroll_step0.1_N_8000_L_100_t_300_taucrawl_100 -m 0.1 -s 300 &
+./colloidgel -N 8000 -L 100 -G 25 -d 0.459 -D 3mM_freeroll_step0.1_N_8000_L_100_t_300_taucrawl_100 -m 0.1 -s 300 &
+./colloidgel -N 8000 -L 100 -G 25 -d 0.6375 -D 1.7mM_freeroll_step0.1_N_8000_L_100_t_300_taucrawl_100 -m 0.1 -s 300 &
+./colloidgel -N 8000 -L 100 -G 25 -d 0.919 -D 0.9mM_freeroll_step0.1_N_8000_L_100_t_300_taucrawl_100 -m 0.1 -s 300 &
+./colloidgel -N 8000 -L 100 -G 25 -d 0.0925 -D 80mM_freeroll_step0.1_N_8000_L_100_t_300_taucrawl_100 -m 0.1 -s 300 &
 wait
